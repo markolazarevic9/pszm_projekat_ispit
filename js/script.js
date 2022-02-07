0ddbf55bb3827ebfaa969f408f10e445f0c1fe17
Marko Lazarevic <mare99lazarevic@gmail.com>
2022-02-07 12:15:37 +0100

    refactor

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-let CHECK = false;
 
 const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
@@ -9,6 +8,12 @@ const btnCalc = document.getElementById("btnCalc");
 const popUp = document.getElementById("popUp");
 const numbers = document.getElementById("numbers");
 const numSum = document.getElementById("numSum");
+
+let n1 = Math.floor(Math.random() * 6);
+let n2 = Math.floor(Math.random() * 6);
+let sum = n1 + n2;
+let q = n1 + " + " + n2 + " ?";
+numbers.textContent = q;
 
 nameInput.addEventListener("change",checkName);
 emailInput.addEventListener("change",checkEmail);
@@ -25,65 +30,9 @@ btn.addEventListener("click", function(e) {
           alert("Niste pravilno popunili polja")
         } else {
           popUp.style.visibility = "visible";
-
-          // $.ajax({
-          //   method: "GET",
-          //   url: "php/ajax.php",
-          //   data: {
-          //     nameInput: nameInput.value,
-          //     emailInput: emailInput.value,
-          //     phoneInput: phoneInput.value
-          //   },
-          //   success: function(odg) {
-          //     alert(odg);
-          //   }
-          // })     
         }
     }
 })
-
-function checkName() {
-    const reg1 = /^([A-Za-z\s']{5,35})$/;
-  
-    if (!reg1.test(nameInput.value)) {
-      nameInput.style.border = "3px solid red";
-      return false;
-    } else {
-      nameInput.style.border = "3px solid green";
-      return true;
-    }
-}
-
-function checkEmail() {
-    const reg2 = /^([A-Za-z0-9_\-\.]+)@([A-Za-z0-9_\-\.]+)\.([A-Za-z]{2,5})$/;
-  
-    if (!reg2.test(emailInput.value)) {
-      emailInput.style.border = "3px solid red";
-      return false;
-    } else {
-      emailInput.style.border = "3px solid green";
-      return true;
-    }
-  }
-
-  function checkPhone() {
-    const reg3 = /^(\+[0-9]{1,3})\ ([0-9]{7,12})$/;
-  
-    if (!reg3.test(phoneInput.value)) {
-      phoneInput.style.border = "3px solid red";
-      return false;
-    } else {
-      phoneInput.style.border = "3px solid green";
-      return true;
-    }
-  }
-
-
-let n1 = Math.floor(Math.random() * 6);
-let n2 = Math.floor(Math.random() * 6);
-let sum = n1 + n2;
-let q = n1 + " + " + n2 + " ?";
-numbers.textContent = q;
 
 btnCalc.addEventListener("click", ()=> {
   let numSumValue = numSum.value;
@@ -108,7 +57,6 @@ btnCalc.addEventListener("click", ()=> {
   }
 })
 
-
 function clearInputs() {
   popUp.style.visibility = "hidden";
   nameInput.style.border = "none";
@@ -117,4 +65,40 @@ function clearInputs() {
   nameInput.value = "";
   phoneInput.value = "";
   emailInput.value = "";
+}
+
+function checkName() {
+  const reg1 = /^([A-Za-z\s']{5,35})$/;
+
+  if (!reg1.test(nameInput.value)) {
+    nameInput.style.border = "3px solid red";
+    return false;
+  } else {
+    nameInput.style.border = "3px solid green";
+    return true;
+  }
+}
+
+function checkEmail() {
+  const reg2 = /^([A-Za-z0-9_\-\.]+)@([A-Za-z0-9_\-\.]+)\.([A-Za-z]{2,5})$/;
+
+  if (!reg2.test(emailInput.value)) {
+    emailInput.style.border = "3px solid red";
+    return false;
+  } else {
+    emailInput.style.border = "3px solid green";
+    return true;
+  }
+}
+
+function checkPhone() {
+  const reg3 = /^(\+[0-9]{1,3})\ ([0-9]{7,12})$/;
+
+  if (!reg3.test(phoneInput.value)) {
+    phoneInput.style.border = "3px solid red";
+    return false;
+  } else {
+    phoneInput.style.border = "3px solid green";
+    return true;
+  }
 }
